ab6be5e659f5313ad2d4e2b4bb3e320764106080
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-04-25 13:29:22 -0700

    Added type definitions to typeDefs

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require("./User");
 const Category = require("./Category");
 const Game = require("./Game")
+const Session = require("./Session")
 
-module.exports = { User, Category, Game };
+module.exports = { User, Category, Game, Session };
---
+++ server/schema/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type Category {
+        _id: ID
+        name: String
+    }
+
+    type Game {
+        _id: ID
+        title: String
+        questions: [Question]
+    }
+
+    type Question {
+        _id: ID
+        question: String
+        correct_answer: String
+        incorrect_answers: [String]
+        difficulty: String
+        type: String
+        category: Category
+    }
+
+    type Session {
+        _id: ID
+        gameDate: String
+        host: User
+        game: Game
+        players: [User]
+    }
+
+    type User {
+        _id: ID
+        firstName: String
+        lastName: String
+        username: String
+        email: String
+        password: String
+        games: [Game]
+    }
+`;
+
+module.exports = typeDefs;
