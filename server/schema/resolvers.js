dc4f9bf581d16c3ea0c88fbb29f069ca541407ad
ggorosave <103039442+ggorosave@users.noreply.github.com>
2023-04-27 08:56:42 -0700

    Added user query to resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User, Category, Game, Session } = require("../models")
 const { signToken } = require("../utils/auth");
+const { populate } = require("../models/User");
 
 const resolvers = {
     Query: {
@@ -9,6 +10,14 @@ const resolvers = {
         },
         game: async (parent, { _id }) => {
             return Game.findById(_id).populate("questions")
+        },
+        user: async (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findById(context.user._id).populate({
+                    path: "games",
+                    populate: "questions"
+                })
+            }
         }
     }
 };
